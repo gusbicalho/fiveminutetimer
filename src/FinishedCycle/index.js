006f0fe0a29e29e1c8b11ce98d075c32d7739f90
Gustavo Bicalho <gusbicalho@gmail.com>
2018-05-01 00:48:41 -0300

    extract CopyToClipboardButton with feedback after copying

--- src/FinishedCycle/index.js
+++ src/FinishedCycle/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { CopyToClipboardButton } from '../CopyToClipboardButton'
 
 const COLLAPSED_STYLE = Object.freeze({
   height: '2em',
@@ -10,9 +10,7 @@ export const FinishedCycle = ({ description, content, toggleCollapse, collapsed 
   <div className='card finished-cycle'>
     <div className='card-header' style={{ display: 'flex' }}>
       <div style={{ flex: 1 }}>{description}</div>
-      <CopyToClipboard text={`${description}\n\n${content}`}>
-        <button className='btn btn-secondary btn-sm'>Copy to Clipboard</button>
-      </CopyToClipboard>
+      <CopyToClipboardButton text={`${description}\n\n${content}`} />
 
       {toggleCollapse &&
         <button className='btn btn-secondary btn-sm' onClick={toggleCollapse}>
