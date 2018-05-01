2bf5bf7c930df909422e21cb772caca52f58849b
Gustavo Bicalho <gusbicalho@gmail.com>
2018-05-01 00:36:57 -0300

    add CopyToClipboard

--- src/FinishedCycle/index.js
+++ src/FinishedCycle/index.js
@@ -1,18 +1,26 @@
 import React from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
+const COLLAPSED_STYLE = Object.freeze({
+  height: '2em',
+  overflow: 'hidden',
+})
 
 export const FinishedCycle = ({ description, content, toggleCollapse, collapsed }) => (
-  <div className='card'>
+  <div className='card finished-cycle'>
     <div className='card-header' style={{ display: 'flex' }}>
       <div style={{ flex: 1 }}>{description}</div>
+      <CopyToClipboard text={`${description}\n\n${content}`}>
+        <button className='btn btn-secondary btn-sm'>Copy to Clipboard</button>
+      </CopyToClipboard>
+
       {toggleCollapse &&
-        <button className='btn btn-secondary' onClick={toggleCollapse}>
-          {collapsed ? 'Show content' : 'Hide content'}
+        <button className='btn btn-secondary btn-sm' onClick={toggleCollapse}>
+          {collapsed ? '+' : '-'}
         </button>}
     </div>
-    {collapsed ||
-      <div className='card-body'>
-        <pre>{content}</pre>
-      </div>
-    }
+    <div className='card-body' style={collapsed ? COLLAPSED_STYLE : undefined}>
+      <pre>{content}</pre>
+    </div>
   </div>
 )
